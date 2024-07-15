4b4f699f02254795d7c6ea13e9f5ead819b9ff0e
Patrick Flügge <patrick.fluegge@mailbox.org>
2024-07-15 07:03:27 +0200

    refactor: remove unused files and folders

--- src/VcardServiceProvider.php
+++ src/VcardServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Schruptor\Vcard;
 
-use Schruptor\Vcard\Commands\VcardCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,8 @@ class VcardServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('vcard')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_vcard_table')
-            ->hasCommand(VcardCommand::class);
+            ->hasConfigFile();
     }
 }
